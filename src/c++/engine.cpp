7f5518f23e7a67e52d85fa990b1c708f5bba1361
philip ross <philip@satchel.scot>
2015-08-05 11:56:47 +0100

    Update for NaN 2.0.0 compatibility

--- src/c++/engine.cpp
+++ src/c++/engine.cpp
@@ -10,7 +10,7 @@
 #define MODE_DECODE 1
 #define ENCODE_FINISH 1
 
-v8::Persistent<v8::FunctionTemplate> Engine::constructor;
+Nan::Persistent<v8::FunctionTemplate> Engine::constructor;
 
 static lzma_stream blank_stream = LZMA_STREAM_INIT;
 
@@ -38,40 +38,40 @@ Engine::~Engine() {
 }
 
 void Engine::Init(v8::Handle<v8::Object> exports) {
-  NanScope();
+  Nan::HandleScope scope;
 
   // constructor template
-  v8::Local<v8::FunctionTemplate> ctor = NanNew<v8::FunctionTemplate>(Engine::New);
-  NanAssignPersistent(constructor, ctor);
-  ctor->SetClassName(NanNew<v8::String>("Engine"));
+  v8::Local<v8::FunctionTemplate> ctor = Nan::New<v8::FunctionTemplate>(Engine::New);
+  constructor.Reset(ctor);
+  ctor->SetClassName(Nan::New<v8::String>("Engine").ToLocalChecked());
   ctor->InstanceTemplate()->SetInternalFieldCount(1);
 
   // prototype methods
   // v8::Local<v8::ObjectTemplate> proto = ctor->PrototypeTemplate();
-  NODE_SET_PROTOTYPE_METHOD(ctor, "close", Engine::Close);
-  NODE_SET_PROTOTYPE_METHOD(ctor, "feed", Engine::Feed);
-  NODE_SET_PROTOTYPE_METHOD(ctor, "drain", Engine::Drain);
+  Nan::SetPrototypeMethod(ctor, "close", Engine::Close);
+  Nan::SetPrototypeMethod(ctor, "feed", Engine::Feed);
+  Nan::SetPrototypeMethod(ctor, "drain", Engine::Drain);
 
-  exports->Set(NanNew<v8::String>("Engine"), ctor->GetFunction());
-  exports->Set(NanNew<v8::String>("MODE_ENCODE"), NanNew<v8::Integer>(MODE_ENCODE));
-  exports->Set(NanNew<v8::String>("MODE_DECODE"), NanNew<v8::Integer>(MODE_DECODE));
-  exports->Set(NanNew<v8::String>("ENCODE_FINISH"), NanNew<v8::Integer>(ENCODE_FINISH));
+  exports->Set(Nan::New<v8::String>("Engine").ToLocalChecked(), ctor->GetFunction());
+  exports->Set(Nan::New<v8::String>("MODE_ENCODE").ToLocalChecked(), Nan::New<v8::Integer>(MODE_ENCODE));
+  exports->Set(Nan::New<v8::String>("MODE_DECODE").ToLocalChecked(), Nan::New<v8::Integer>(MODE_DECODE));
+  exports->Set(Nan::New<v8::String>("ENCODE_FINISH").ToLocalChecked(), Nan::New<v8::Integer>(ENCODE_FINISH));
 }
 
 // new Engine(MODE_DECODE or MODE_ENCODE, [ preset ]);
 // "preset" is the compression level (0 - 9)
 NAN_METHOD(Engine::New) {
-  NanScope();
+  Nan::HandleScope scope;
 
-  if (!args.IsConstructCall()) {
-    NanThrowError("Must be called as constructor");
+  if (!info.IsConstructCall()) {
+    Nan::ThrowError("Must be called as constructor");
   }
 
-  int mode = (args.Length() > 0) ? args[0]->IntegerValue() : 0;
-  int preset = (args.Length() > 1) ? args[1]->IntegerValue() : 6;
+  int mode = (info.Length() > 0) ? info[0]->IntegerValue() : 0;
+  int preset = (info.Length() > 1) ? info[1]->IntegerValue() : 6;
 
   Engine *obj = new Engine();
-  obj->Wrap(args.This());
+  obj->Wrap(info.This());
 
   lzma_ret ret;
   if (mode == MODE_DECODE) {
@@ -81,69 +81,69 @@ NAN_METHOD(Engine::New) {
   }
   if (ret != LZMA_OK) {
     delete obj;
-    NanThrowError(lzma_perror(ret));
+    Nan::ThrowError(lzma_perror(ret));
   }
 
   obj->_active = true;
-  NanReturnThis();
+  info.GetReturnValue().Set(info.This());
 }
 
 NAN_METHOD(Engine::Close) {
-  NanScope();
+  Nan::HandleScope scope;
 
-  Engine *obj = ObjectWrap::Unwrap<Engine>(args.This());
-  if (!obj->_active) NanThrowError("Engine has already been closed");
+  Engine *obj = Nan::ObjectWrap::Unwrap<Engine>(info.This());
+  if (!obj->_active) Nan::ThrowError("Engine has already been closed");
 
   lzma_end(&obj->_stream);
   obj->_active = false;
-  NanReturnUndefined();
+  return;
 }
 
 // prep next "Drain" by setting up the input buffer.
 // you may not let the buffer leave scope before draining!
 NAN_METHOD(Engine::Feed) {
-  NanScope();
+  Nan::HandleScope scope;
 
-  Engine *obj = ObjectWrap::Unwrap<Engine>(args.This());
-  if (!obj->_active) NanThrowError("Engine has already been closed");
+  Engine *obj = Nan::ObjectWrap::Unwrap<Engine>(info.This());
+  if (!obj->_active) Nan::ThrowError("Engine has already been closed");
 
-  if (args.Length() != 1) NanThrowTypeError("Requires 1 argument: <buffer>");
+  if (info.Length() != 1) Nan::ThrowTypeError("Requires 1 argument: <buffer>");
 
-  v8::Handle<v8::Object> buffer = args[0]->ToObject();
-  if (!node::Buffer::HasInstance(buffer)) NanThrowTypeError("Argument must be a buffer");
+  v8::Handle<v8::Object> buffer = info[0]->ToObject();
+  if (!node::Buffer::HasInstance(buffer)) Nan::ThrowTypeError("Argument must be a buffer");
   obj->_stream.next_in = (const uint8_t *) node::Buffer::Data(buffer);
   obj->_stream.avail_in = node::Buffer::Length(buffer);
 
-  NanReturnValue(NanNew<v8::Integer>((int) obj->_stream.avail_in));
+  info.GetReturnValue().Set(Nan::New<v8::Integer>((int) obj->_stream.avail_in));
 }
 
 // run the encoder, filling as much of the buffer as possible, returning the amount used.
 // negative value means you need to run it again.
 // if "finished" is true, tell the encoder there will be no more data, and to wrap it up.
 NAN_METHOD(Engine::Drain) {
-  NanScope();
+  Nan::HandleScope scope;
 
-  Engine *obj = ObjectWrap::Unwrap<Engine>(args.This());
-  if (!obj->_active) NanThrowError("Engine has already been closed");
-  if (args.Length() < 1 || args.Length() > 2) NanThrowTypeError("Requires 1 or 2 arguments: <buffer> [<flags>]");
+  Engine *obj = Nan::ObjectWrap::Unwrap<Engine>(info.This());
+  if (!obj->_active) Nan::ThrowError("Engine has already been closed");
+  if (info.Length() < 1 || info.Length() > 2) Nan::ThrowTypeError("Requires 1 or 2 arguments: <buffer> [<flags>]");
 
-  v8::Handle<v8::Object> buffer = args[0]->ToObject();
-  if (!node::Buffer::HasInstance(buffer)) NanThrowTypeError("Argument must be a buffer");
+  v8::Handle<v8::Object> buffer = info[0]->ToObject();
+  if (!node::Buffer::HasInstance(buffer)) Nan::ThrowTypeError("Argument must be a buffer");
 
-  int flags = (args.Length() > 1) ? args[1]->IntegerValue() : 0;
+  int flags = (info.Length() > 1) ? info[1]->IntegerValue() : 0;
 
   lzma_action action = (flags & ENCODE_FINISH) ? LZMA_FINISH : LZMA_RUN;
 
   obj->_stream.next_out = (uint8_t *) node::Buffer::Data(buffer);
   obj->_stream.avail_out = node::Buffer::Length(buffer);
   lzma_ret ret = lzma_code(&obj->_stream, action);
-  if (ret != LZMA_OK && ret != LZMA_STREAM_END) NanThrowError(lzma_perror(ret));
+  if (ret != LZMA_OK && ret != LZMA_STREAM_END) Nan::ThrowError(lzma_perror(ret));
 
   int used = node::Buffer::Length(buffer) - obj->_stream.avail_out;
   if (obj->_stream.avail_in > 0 || (action == LZMA_FINISH && ret != LZMA_STREAM_END)) {
     // try more.
-    NanReturnValue(NanNew<v8::Integer>(-used));
+    info.GetReturnValue().Set(Nan::New<v8::Integer>(-used));
   } else {
-    NanReturnValue(NanNew<v8::Integer>(used));
+    info.GetReturnValue().Set(Nan::New<v8::Integer>(used));
   }
 }
