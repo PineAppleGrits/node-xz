551cab6f263123e98c97adfe614b23f9c44215fd
Robey Pointer <robeypointer@gmail.com>
2018-12-08 12:09:44 -0800

    use new callback-oriented c++ api for the transform

--- src/test/test_engine.ts
+++ src/test/test_engine.ts
@@ -10,64 +10,110 @@ describe("Engine", () => {
     (() => engine.close()).should.throw(/closed/);
   });
 
-  it("encodes in steps", () => {
+  it("encodes in steps", (done) => {
     const writer = compressRaw(6);
     const b1 = Buffer.alloc(128);
-    const n1 = writer.process(Buffer.from("hello, hello!"), b1);
-    n1.should.eql(24);
+    writer.process(Buffer.from("hello, hello!"), b1, 0, (error, used) => {
+      (error === undefined).should.eql(true);
+      (used || 0).should.eql(24);
 
-    const b2 = Buffer.alloc(128);
-    const n2 = writer.process(undefined, b2, ENCODE_FINISH);
-    n2.should.eql(40);
+      const b2 = Buffer.alloc(128);
+      writer.process(undefined, b2, ENCODE_FINISH, (error, used) => {
+        (error === undefined).should.eql(true);
+        (used || 0).should.eql(40);
+        done();
+      });
+    });
   });
 
-  it("encodes all at once", () => {
+  it("encodes all at once", (done) => {
     const writer = compressRaw(6);
     const b1 = Buffer.alloc(128);
-    const n1 = writer.process(Buffer.from("hello, hello!"), b1, ENCODE_FINISH);
-    n1.should.eql(64);
+    writer.process(Buffer.from("hello, hello!"), b1, ENCODE_FINISH, (error, used) => {
+      (error === undefined).should.eql(true);
+      (used || 0).should.eql(64);
+      done();
+    });
   });
 
-  it("copes with insufficient space", () => {
+  it("copes with insufficient space", (done) => {
     const writer = compressRaw(6);
     const b1 = Buffer.alloc(32);
-    const n1 = writer.process(Buffer.from("hello, hello!"), b1, ENCODE_FINISH);
-    n1.should.eql(-32);
-    const b2 = Buffer.alloc(32);
-    const n2 = writer.process(undefined, b2, ENCODE_FINISH);
-    n2.should.eql(32);
+    writer.process(Buffer.from("hello, hello!"), b1, ENCODE_FINISH, (error, used) => {
+      (error === undefined).should.eql(true);
+      (used || 0).should.eql(-32);
 
-    const fullWriter = compressRaw(6);
-    const bf = Buffer.alloc(64);
-    const nf = fullWriter.process(Buffer.from("hello, hello!"), bf, ENCODE_FINISH);
-    nf.should.eql(64);
-    Buffer.concat([ b1, b2 ]).should.eql(bf);
+      const b2 = Buffer.alloc(32);
+      writer.process(undefined, b2, ENCODE_FINISH, (error, used) => {
+        (error === undefined).should.eql(true);
+        (used || 0).should.eql(32);
+
+        const fullWriter = compressRaw(6);
+        const bf = Buffer.alloc(64);
+        fullWriter.process(Buffer.from("hello, hello!"), bf, ENCODE_FINISH, (error, used) => {
+          (error === undefined).should.eql(true);
+          (used || 0).should.eql(64);
+          Buffer.concat([ b1, b2 ]).should.eql(bf);
+          done();
+        });
+      });
+    });
   });
 
-  it("can decode what it encodes", () => {
+  it("can decode what it encodes", (done) => {
     const writer = compressRaw(6);
     const b1 = Buffer.alloc(128);
-    const n1 = writer.process(Buffer.from("hello, hello!"), b1, ENCODE_FINISH);
-    writer.close();
+    writer.process(Buffer.from("hello, hello!"), b1, ENCODE_FINISH, (error, used) => {
+      (error === undefined).should.eql(true);
 
-    const reader = decompressRaw();
-    let b2 = Buffer.alloc(128);
-    const n2 = reader.process(b1.slice(0, n1), b2);
-    b2.slice(0, n2).toString().should.eql("hello, hello!");
+      const reader = decompressRaw();
+      let b2 = Buffer.alloc(128);
+      reader.process(b1.slice(0, used), b2, 0, (error, used) => {
+        b2.slice(0, used).toString().should.eql("hello, hello!");
+        writer.close();
+        done();
+      });
+    });
   });
 
-  it("encodes into an offset", () => {
+  it("encodes into an offset", (done) => {
     const buffer = Buffer.alloc(64);
     Buffer.from("hello").copy(buffer);
 
     const writer = compressRaw(6);
-    writer.process(buffer.slice(0, 5), buffer.slice(8, 64), ENCODE_FINISH).should.eql(56);
+    writer.process(buffer.slice(0, 5), buffer.slice(8, 64), ENCODE_FINISH, (error, used) => {
+      (error === undefined).should.eql(true);
+      (used || 0).should.eql(56);
 
-    buffer[0] = 0;
-    buffer[2] = 9;
+      buffer[0] = 0;
+      buffer[2] = 9;
 
-    const reader = decompressRaw();
-    reader.process(buffer.slice(8, 64), buffer.slice(0, 8)).should.eql(5);
-    buffer.slice(0, 5).toString().should.eql("hello");
+      const reader = decompressRaw();
+      reader.process(buffer.slice(8, 64), buffer.slice(0, 8), 0, (error, used) => {
+        (error === undefined).should.eql(true);
+        (used || 0).should.eql(5);
+
+        buffer.slice(0, 5).toString().should.eql("hello");
+        done();
+      });
+    });
+  });
+
+  it("can't close twice", () => {
+    const writer = compressRaw(6);
+    writer.close();
+    (() => writer.close()).should.throw(/already been closed/);
+  });
+
+  it("can't be used twice in parallel", () => {
+    const inBuffer = Buffer.alloc(65536);
+    const outBuffer = Buffer.alloc(65536);
+    const writer = compressRaw(6);
+    writer.process(inBuffer, outBuffer, ENCODE_FINISH, (error, used) => {
+      // don't care.
+    });
+    (() => writer.process(inBuffer, outBuffer, ENCODE_FINISH, (error, used) => {
+      // don't care.
+    })).should.throw(/is in use/);
   });
 });
--- src/xz.ts
+++ src/xz.ts
@@ -2,15 +2,21 @@ import * as stream from "stream";
 
 const node_xz = require("../build/Release/node_xz.node");
 
+const DEFAULT_PRESET = 6;
+
 const MIN_BUFSIZE = 1024;
 const MAX_BUFSIZE = 64 * 1024;
 
+export type EngineCallback = (error?: Error, used?: number) => void;
+
 export interface Engine {
   close(): void;
-  process(input: Buffer | undefined, output: Buffer, flags?: number): number;
+  process(input: Buffer | undefined, output: Buffer, flags: number, callback: EngineCallback): number;
 }
 
 export const ENCODE_FINISH = node_xz.ENCODE_FINISH;
+export const MODE_ENCODE = node_xz.MODE_ENCODE;
+export const MODE_DECODE = node_xz.MODE_DECODE;
 
 export interface XzTransformOptions extends stream.TransformOptions {
   preset?: number;
@@ -23,63 +29,58 @@ class XzStream extends stream.Transform {
   // keep one buffer around in case we can reuse it between calls.
   buffer: Buffer;
 
-  constructor(mode?: number, options?: XzTransformOptions) {
+  constructor(mode: number, options?: XzTransformOptions) {
     super(options);
-    this.engine = new node_xz.Engine(mode, options ? options.preset : undefined);
+    this.engine = new node_xz.Engine(mode, options ? options.preset : DEFAULT_PRESET);
     this.buffer = Buffer.alloc(options && options.bufferSize ? options.bufferSize : MIN_BUFSIZE);
   }
 
   _transform(chunk: Buffer | string, encoding: string | undefined, callback: stream.TransformCallback) {
-    const rv = this.process(chunk instanceof Buffer ? chunk : Buffer.from(chunk, encoding));
-    this.push(rv);
-    callback(undefined);
+    const input = chunk instanceof Buffer ? chunk : Buffer.from(chunk, encoding);
+    const bufSize = Math.max(Math.min(input.length * 1.1, MAX_BUFSIZE), MIN_BUFSIZE);
+    if (bufSize > this.buffer.length) this.buffer = Buffer.alloc(bufSize);
+    this.processLoop(input, 0, [], callback);
   }
 
   _flush(callback: stream.TransformCallback) {
-    const rv = this.process(undefined, node_xz.ENCODE_FINISH);
-    this.push(rv);
-    callback(undefined);
+    this.processLoop(undefined, node_xz.ENCODE_FINISH, [], callback);
   }
 
-  process(input: Buffer | undefined, flags?: number): Buffer {
+  processLoop(input: Buffer | undefined, flags: number, segments: Buffer[], callback: stream.TransformCallback) {
     // slightly too clever: we use the same buffer each time, as long as it's
     // big enough: the `concat` at the end will create a new one. if we need
     // to go back for a 2nd+ time, we replace it, since the array of `slice`
     // are just views.
-    const bufSize = Math.max(Math.min(input ? input.length * 1.1 : this.buffer.length, MAX_BUFSIZE), MIN_BUFSIZE);
-    if (bufSize > this.buffer.length) this.buffer = Buffer.alloc(bufSize);
-
-    let n = this.engine.process(input, this.buffer, flags);
-    let size = Math.abs(n);
-    const segments = [ this.buffer.slice(0, size) ];
-
-    while (n < 0) {
-      this.buffer = Buffer.alloc(this.buffer.length);
-      n = this.engine.process(undefined, this.buffer, flags);
-      size += Math.abs(n);
-      segments.push(this.buffer.slice(0, Math.abs(n)));
-    }
-
-    return Buffer.concat(segments, size);
+    this.engine.process(input, this.buffer, flags, (error, used) => {
+      if (error || used === undefined) return callback(error || new Error("Unknown error"));
+      let size = Math.abs(used);
+      segments.push(this.buffer.slice(0, size));
+      if (used < 0) {
+        this.buffer = Buffer.alloc(this.buffer.length);
+        this.processLoop(undefined, flags, segments, callback);
+      } else {
+        callback(undefined, Buffer.concat(segments));
+      }
+    });
   }
 }
 
 export class Compressor extends XzStream {
   constructor(options?: XzTransformOptions) {
-    super(node_xz.MODE_ENCODE, options);
+    super(MODE_ENCODE, options);
   }
 }
 
 export class Decompressor extends XzStream {
   constructor(options?: XzTransformOptions) {
-    super(node_xz.MODE_DECODE, options);
+    super(MODE_DECODE, options);
   }
 }
 
-export function compressRaw(preset?: number): Engine {
-  return new node_xz.Engine(node_xz.MODE_ENCODE, preset);
+export function compressRaw(preset: number = DEFAULT_PRESET): Engine {
+  return new node_xz.Engine(MODE_ENCODE, preset);
 }
 
 export function decompressRaw(): Engine {
-  return new node_xz.Engine(node_xz.MODE_DECODE);
+  return new node_xz.Engine(MODE_DECODE, 0);
 }
