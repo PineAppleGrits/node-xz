af25028cb3d9334a23fca3ba0ca1632f803d92d2
Robey Pointer <robeypointer@gmail.com>
2018-12-08 12:08:53 -0800

    rewrite the core of the c++ engine to use an async worker for the call to lzma

--- src/c++/engine.cpp
+++ src/c++/engine.cpp
@@ -23,6 +23,8 @@ static const char *lzma_perror(lzma_ret err) {
 }
 
 
+// ----- Engine
+
 Napi::Object Engine::Init(Napi::Env env, Napi::Object exports) {
   Napi::HandleScope scope(env);
 
@@ -43,15 +45,18 @@ Napi::Object Engine::Init(Napi::Env env, Napi::Object exports) {
 
 // new Engine(MODE_DECODE or MODE_ENCODE, [ preset ]);
 // "preset" is the compression level (0 - 9)
-Engine::Engine(const Napi::CallbackInfo& info) : Napi::ObjectWrap<Engine>(info) {
+Engine::Engine(const Napi::CallbackInfo& info) :
+  Napi::ObjectWrap<Engine>(info), stream(blank_stream), active(false), closed(true) {
   Napi::Env env = info.Env();
   Napi::HandleScope scope(env);
 
-  stream = blank_stream;
-  active = false;
+  if (info.Length() < 2 || !info[0].IsNumber() || !info[1].IsNumber()) {
+    Napi::TypeError::New(env, "Requires 2 arguments: <mode> <preset>").ThrowAsJavaScriptException();
+    return;
+  }
 
-  int mode = (info.Length() > 0 && !info[0].IsUndefined()) ? info[0].As<Napi::Number>().Int32Value() : 0;
-  int preset = (info.Length() > 1 && !info[1].IsUndefined()) ? info[1].As<Napi::Number>().Int32Value() : 6;
+  int mode = info[0].As<Napi::Number>().Int32Value();
+  int preset = info[1].As<Napi::Number>().Int32Value();
 
   lzma_ret ret;
   if (mode == MODE_DECODE) {
@@ -64,7 +69,7 @@ Engine::Engine(const Napi::CallbackInfo& info) : Napi::ObjectWrap<Engine>(info) 
     return;
   }
 
-  active = true;
+  closed = false;
 }
 
 Engine::~Engine() {
@@ -72,29 +77,45 @@ Engine::~Engine() {
 }
 
 Napi::Value Engine::Close(const Napi::CallbackInfo& info) {
-  if (!active) {
+  if (closed) {
     Napi::Error::New(info.Env(), "Engine has already been closed").ThrowAsJavaScriptException();
+    return Napi::Value();
+  }
+  if (active) {
+    Napi::Error::New(info.Env(), "Engine is in use").ThrowAsJavaScriptException();
     return Napi::Value();
   }
 
   lzma_end(&stream);
-  active = false;
+  inBufferHandle.Reset();
+  closed = true;
   return info.Env().Undefined();
 }
 
 Napi::Value Engine::Process(const Napi::CallbackInfo& info) {
-  if (!active) {
+  Napi::HandleScope scope(info.Env());
+
+  if (closed) {
     Napi::Error::New(info.Env(), "Engine has already been closed").ThrowAsJavaScriptException();
-    buffer_ref.Reset();
+    return Napi::Value();
+  }
+  if (active) {
+    Napi::Error::New(info.Env(), "Engine is in use").ThrowAsJavaScriptException();
     return Napi::Value();
   }
 
-  if (info.Length() < 2 || !info[1].IsBuffer() || !(info[0].IsBuffer() || info[0].IsUndefined())) {
+  if (
+    info.Length() < 4 ||
+    !info[1].IsBuffer() ||
+    !(info[0].IsBuffer() || info[0].IsUndefined()) ||
+    !info[2].IsNumber() ||
+    !info[3].IsFunction()
+  ) {
     Napi::Error::New(
       info.Env(),
-      "Requires 2 or 3 arguments: <buffer|undefined> <buffer> [flags]"
+      "Requires 4 arguments: <buffer|undefined> <buffer> <flags> <callback>"
     ).ThrowAsJavaScriptException();
-    buffer_ref.Reset();
+    inBufferHandle.Reset();
     return Napi::Value();
   }
 
@@ -103,31 +124,50 @@ Napi::Value Engine::Process(const Napi::CallbackInfo& info) {
     stream.next_in = (const uint8_t *) buffer.Data();
     stream.avail_in = buffer.Length();
     // keep a copy:
-    buffer_ref.Reset(buffer, 1);
-  } else if (buffer_ref.IsEmpty()) {
+    inBufferHandle.Reset(buffer, 1);
+  } else if (inBufferHandle.IsEmpty()) {
     stream.next_in = nullptr;
     stream.avail_in = 0;
   }
 
   Napi::Buffer<uint8_t> buffer = info[1].As<Napi::Buffer<uint8_t>>();
-  int flags = (info.Length() > 2 && info[2].IsNumber()) ? info[2].As<Napi::Number>().Int32Value() : 0;
+  int flags = info[2].As<Napi::Number>().Int32Value();
+  Napi::Function callback = info[3].As<Napi::Function>();
 
   lzma_action action = (flags & ENCODE_FINISH) ? LZMA_FINISH : LZMA_RUN;
-  stream.next_out = (uint8_t *) buffer.Data();
+  stream.next_out = buffer.Data();
   stream.avail_out = buffer.Length();
+  outBufferHandle.Reset(buffer, 1);
 
-  lzma_ret ret = lzma_code(&stream, action);
+  // deleted via magic! inside AsyncWorker
+  AsyncEngine *e = new AsyncEngine(callback, this, action);
+  e->Queue();
+  active = true;
+  return info.Env().Undefined();
+}
+
+
+// ----- AsyncEngine
+
+// happens on a worker thread
+void AsyncEngine::Execute(void) {
+  size_t len = engine->stream.avail_out;
+  ret = lzma_code(&engine->stream, action);
   if (ret != LZMA_OK && ret != LZMA_STREAM_END) {
-    Napi::Error::New(info.Env(), lzma_perror(ret)).ThrowAsJavaScriptException();
-    return Napi::Value();
+    SetError(lzma_perror(ret));
+    return;
   }
 
-  int used = buffer.Length() - stream.avail_out;
-  if (stream.avail_in > 0 || (action == LZMA_FINISH && ret != LZMA_STREAM_END)) {
-    // need more space!
-    return Napi::Number::New(info.Env(), -used);
-  } else {
-    buffer_ref.Reset();
-    return Napi::Number::New(info.Env(), used);
-  }
+  used = len - engine->stream.avail_out;
+}
+
+// back in the js thread
+void AsyncEngine::OnOK(void) {
+  Napi::HandleScope scope(Env());
+  bool finished = engine->stream.avail_in == 0 && (ret == LZMA_STREAM_END || action != LZMA_FINISH);
+  if (finished) engine->inBufferHandle.Reset();
+  engine->outBufferHandle.Reset();
+  engine->active = false;
+  // 0 is ok here. if it used 0 bytes, it's definitely finished for this block.
+  Callback().Call({ Env().Undefined(), Napi::Number::New(Env(), finished ? (double)used : -(double)used) });
 }
--- src/c++/engine.h
+++ src/c++/engine.h
@@ -3,6 +3,8 @@
 #include <napi.h>
 #include "lzma.h"
 
+typedef Napi::Buffer<uint8_t> NapiByteBuffer;
+
 /**
  * This low-level wrapper is unpleasant, and isn't meant to be used directly.
  * It just provides access to the raw stream engine in xz.
@@ -12,13 +14,24 @@ public:
   static Napi::Object Init(Napi::Env env, Napi::Object exports);
 
   explicit Engine(const Napi::CallbackInfo& info);
-  ~Engine();
+  virtual ~Engine();
+
+  lzma_stream stream;
+  bool active;  // true while the engine is running an AsyncWorker
+
+  // keep a handle to the input buffer if we expect to be called again
+  // with the same input because the original output buffer wasn't big enough.
+  Napi::Reference<NapiByteBuffer> inBufferHandle;
+
+  // keep a handle to the output buffer across the thread call, to make sure
+  // it isn't GC'd.
+  Napi::Reference<NapiByteBuffer> outBufferHandle;
 
 private:
   // close()
   Napi::Value Close(const Napi::CallbackInfo& info);
 
-  // process(input: Buffer | undefined, output: Buffer): number
+  // process(input: Buffer | undefined, output: Buffer, callback: (used: number) => void)
   //   - returns a positive number (how much output was used) if the complete
   //     input was processed
   //   - returns a negative number (how much output was used, negative) if
@@ -28,10 +41,28 @@ private:
 
   static Napi::FunctionReference constructor;
 
-  lzma_stream stream;
-  bool active;
+  bool closed;
+};
 
-  // keep a handle to the input buffer if we expect to be called again
-  // with the same input because the original output buffer wasn't big enough.
-  Napi::Reference<Napi::Buffer<uint8_t>> buffer_ref;
+
+class AsyncEngine : public Napi::AsyncWorker {
+public:
+  // c++ is ridiculous.
+  AsyncEngine(Napi::Function& callback, Engine *engine, lzma_action action) :
+    AsyncWorker(callback), engine(engine), action(action), used(0) {
+    // pass
+  }
+
+  virtual ~AsyncEngine() {
+    // pass
+  }
+
+  virtual void Execute(void);
+  virtual void OnOK(void);
+
+private:
+  Engine *engine;
+  lzma_action action;
+  size_t used;
+  lzma_ret ret;
 };
